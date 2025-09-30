9ceb290c655ae85268b3450cb4458ccc538cc227
kobyrthr@gmail.com <kobinaarthur@Kobinas-MacBook-Air.local>
2025-09-30 11:14:32 -0500

    Added refresh and redirect actions.

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,6 +1,8 @@
 'use server';
  
 import {z} from 'zod';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
 import postgres from 'postgres';
 
 const sql = postgres(process.env.POSTGRES_URL!, {ssl: 'require'})
@@ -25,8 +27,10 @@ export async function createInvoice(formData: FormData) {
     const date = new Date().toISOString().split('T')[0];
 
     await sql`
-    INSERT INTO invoice (customer_id, amount, status, date)
+    INSERT INTO invoices (customer_id, amount, status, date)
     VALUES (${customerId},${amountInCents},${status},${date})
-    `
+    `;
 
+    revalidatePath('/dashboard/invoices');
+    redirect('/dashboard/invoices');
 }
